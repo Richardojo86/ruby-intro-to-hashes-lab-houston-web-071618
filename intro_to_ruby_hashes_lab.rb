77aeab85048b2a9351ec7db25f3bc09f5f1fc705
richardojo04@gmail.com <richardojo04@gmail.com>
2018-06-20 05:39:47 +0000

    automatically backed up by learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -31,8 +31,10 @@ monopoly = {
   :railroads => {
   :pieces => 4,
   :names => {
-    :reading_railroad => {}
-    
+  :reading_railroad => {},
+  :pennsylvania_railroad => {},
+  :b_and_o_railroad => {},
+  :shortline => {}
   },
   
   :rent_in_dollars => {
