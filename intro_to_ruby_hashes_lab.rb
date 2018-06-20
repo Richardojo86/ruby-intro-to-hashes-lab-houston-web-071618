5be479d6d2fb2fafdeec08a70925625fc20531fd
richardojo04@gmail.com <richardojo04@gmail.com>
2018-06-20 04:32:12 +0000

    automatically backed up by learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -17,7 +17,7 @@ def monopoly_with_second_tier
 monopoly[:railroads] = {}
 monopoly[:railroads][:pieces] = 4
 monopoly[:railroads][:names] = {}
-mono
+monopoly[:railroads][:rent_in_dollars] = {}
 	
 end
 
